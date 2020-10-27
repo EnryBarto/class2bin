9d20334953285ec23af5c1b34c63a11f7375757e
Tommaso <tommaso.andreanelli@icloud.com>
2020-10-27 09:00:34 +0100

    fixed registro elettronico

--- assets/js/cvv.js
+++ assets/js/cvv.js
@@ -1,3 +1,3 @@
 function load_cvv() {
-	document.getElementById("cvv").src = "https://web.spaggiari.eu/home/app/default/login.php?ch=scuola";
+	document.getElementById("cvv").src = "https://web.spaggiari.eu/home/app/default/login.php";
 }
